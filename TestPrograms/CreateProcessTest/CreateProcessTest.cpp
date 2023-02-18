91f098dd27ad287cf04f95fcc683735da6b7d725
frstb0mb <httrx1286@gmail.com>
2023-02-18 21:34:00 +0900

    Add Tests

---
+++ TestPrograms/CreateProcessTest/CreateProcessTest.cpp
@@ -0,0 +1,13 @@
+﻿#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+int main()
+{
+    wchar_t cmd[] = L"notepad";
+    STARTUPINFOW si = {};
+    PROCESS_INFORMATION pi = {};
+    CreateProcessW(nullptr, cmd, nullptr, nullptr, FALSE, 0, nullptr, nullptr, &si, &pi);
+    WaitForSingleObject(pi.hProcess, 1000);
+    CloseHandle(pi.hProcess);
+    CloseHandle(pi.hThread);
+}
