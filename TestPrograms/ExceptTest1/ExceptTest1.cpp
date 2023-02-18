3358bfd82dd91bc33e5868c550fad20c9b646f9e
frstb0mb <16902648+frstb0mb@users.noreply.github.com>
2023-02-18 21:34:00 +0900

    Add Tests

---
+++ TestPrograms/ExceptTest1/ExceptTest1.cpp
@@ -0,0 +1,15 @@
+﻿#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#include <stdio.h>
+
+int main()
+{
+    int y = 0;
+    __try {
+        auto x = 1 / y;
+        printf("%d\n", x);
+    }
+    __except (EXCEPTION_EXECUTE_HANDLER) {
+        printf("Zero Divided\n");
+    }
+}
---
+++ TestPrograms/ExceptTest2/ExceptTest2.cpp
@@ -0,0 +1,25 @@
+﻿#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#include <stdio.h>
+
+LONG WINAPI Handler(struct _EXCEPTION_POINTERS* ExceptionInfo) {
+    printf("[EXCEPTIONHANDLER] VEH\n");
+    return EXCEPTION_CONTINUE_SEARCH;
+}
+
+int main()
+{
+    AddVectoredExceptionHandler(1, Handler);
+    auto mem = VirtualAlloc(nullptr, 0x1000, MEM_COMMIT, PAGE_READONLY);
+    if (!mem) {
+        printf("Failed\n");
+        return 0;
+    }
+
+    __try {
+        *(PBYTE)mem = 1;
+    }
+    __except (EXCEPTION_EXECUTE_HANDLER) {
+        printf("[EXCEPTIONHANDLER] SEH\n");
+    }
+}
